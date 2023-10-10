be11cb5bf5c3b96ca1bbfeab3e6c4424bd5efe4d
Suketug <suketu.gaglani@gmail.com>
2023-10-10 15:50:01 -0500

    u

--- ai_chat/Code/retriever.py
+++ ai_chat/Code/retriever.py
@@ -5,13 +5,20 @@ from sentence_transformers import SentenceTransformer
 from logger import setup_logger
 import logging
 import time
+import nltk
+from nltk.corpus import stopwords
+
+# Initialize the set of stopwords
+stop_words = set(stopwords.words('english'))
+
+# Function to simplify queries by removing stop words
 
 # Set up the logger
 logger = setup_logger('retriever', level=logging.INFO)
 
 # Initialize Persistent Chroma Client and other global variables only once
 try:
-    chroma_client = chromadb.PersistentClient(path="../Extracted_Data/Chroma_Db")
+    chroma_client = chromadb.PersistentClient(path="../Extracted_Data/Chroma_Db/ChromaDB")
     sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
     collection = chroma_client.get_or_create_collection(name="mortgage_data", embedding_function=sentence_transformer_ef, metadata={"hnsw:space": "cosine"})
     embedder = SentenceTransformer("all-MiniLM-L6-v2")
@@ -20,26 +27,47 @@ except Exception as e:
     logger.exception("Error initializing ChromaDB client:")
     raise e
 
-def retrieve(query, k=5, column=None):
+def simplify_query(query):
+    query_words = query.split()
+    simplified_query = ' '.join([word for word in query_words if word.lower() not in stop_words])
+    return simplified_query
+
+def retrieve(query, k=5):
     try:
-        where_filter = {"column": column} if column else None
+        # Simplifying the query
+        simplified_query = simplify_query(query)
+
+        # Ensure the query is conducted on the Cleaned_Description column
+        where_filter = {"column": "summarized_text"}
+
+        # Convert query text to an embedding
+        query_embedding = embedder.encode([query])[0].tolist()  # Convert NumPy array to list
+        
+        # Perform the query on the mortgage_data collection
         results = collection.query(
-            query_texts=[query],
+            query_embeddings=[query_embedding],  # Use the query embedding
             n_results=k,
-            where=where_filter
+            where=where_filter  # Apply the where filter
         )
 
+        # Check for empty or unexpected results
         if not results or 'metadatas' not in results:
             logger.warning("Query returned empty or unexpected results")
             return []
 
-        combined_strings = [f"{res.get('Cleaned_Description', '')}. {res.get('FAQ_Answers', '')}" for res in results['metadatas'][0]]
-        logger.info(f"Retrieved results for query: {query}")
-
+        # Combine the Cleaned_Description and FAQ_Answers fields from the results
+        combined_strings = [
+            f"{res.get('Cleaned_Description', '')}. {res.get('FAQ_Answers', '')}" 
+            for res in results['metadatas'][0]
+        ]
+        
+        logger.info(f"Retrieved results for query: {simplified_query}")
         return combined_strings
+        
     except Exception as e:
         logger.exception("Error in retrieval:")
         return []
+
 
 def get_highest_similarity_score(query, caller="Unknown"):
     try:
